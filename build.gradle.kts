d69ab0c9bc8433d28bd84973d4a370d7e72be236
Lucas Lima T <lukaslymauchiha@gmail.com>
2020-04-14 12:48:22 -0300

    Alteração do build.gradle(Nivel Projeto) para kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath(GradlePlugins.ANDROID_GRADLE)
+        classpath(GradlePlugins.KOTLIN_GRADLE)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
